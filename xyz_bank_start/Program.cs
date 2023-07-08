d3f5086846e8d54a5934fa3910c9db815502149f
TkAtHSO <tanmaya.kulkarni@henryschein.com>
2023-07-08 11:46:51 +0100

    Few tweaks

--- xyz_bank_start/Program.cs
+++ xyz_bank_start/Program.cs
@@ -3,17 +3,19 @@ using System.Security.Principal;
 
 var bank = new Bank();
 var checkingAccount = new Account(Account.CHECKING);
-//var savingsAccount = new Account(Account.SAVINGS);
+var savingsAccount = new Account(Account.SAVINGS);
+var maxiSavingsAccount = new Account(Account.MAXI_SAVINGS);
 
-//var henry = new Customer("Henry").OpenAccount(checkingAccount).OpenAccount(savingsAccount);
-var henry = new Customer("Henry").OpenAccount(checkingAccount);
+var henry = new Customer("Henry").OpenAccount(checkingAccount).OpenAccount(savingsAccount).OpenAccount(maxiSavingsAccount);
 bank.AddCustomer(henry);
 
 checkingAccount.Deposit(100.0);
-//savingsAccount.Deposit(4000.0);
-//savingsAccount.Withdraw(200.0);
+savingsAccount.Deposit(1200.0);
+
+savingsAccount.Withdraw(100.0);
+maxiSavingsAccount.Withdraw(100);
 
 
 var interest = bank.totalInterestPaid();
 Console.WriteLine($"Total interest for {henry.GetName()}: {interest}");
-//Console.Write(henry.GetStatement());
+Console.Write(henry.GetStatement());
